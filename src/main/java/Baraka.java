ce0d7be9145458fa4c9e129929b6fe89d429c9a0
SheLesTT <lovsevdenis3513@gmail.com>
2023-05-17 13:44:43 +0300

    final

---
+++ src/main/java/Baraka.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Мария
+ */
+
+// танк
+public class Baraka extends Player{
+    
+    public Baraka(int level, int health, int  damage, int attack){
+        super (level, health, damage, attack);
+    }
+    
+    @Override
+    public String getName(){
+        return "Baraka";
+    }
+    
+}
---
+++ src/main/java/Location.java
@@ -0,0 +1,21 @@
+//public class Location {
+//    int human_lvl;
+//
+//    Fight fight = new Fight();
+//    public Location(int human_level, Fight fight){
+//        this.human_lvl = human_level;
+//    }
+//    Player[] enemies;
+//
+//    private int count_num_enemies(){
+//        return  (int)(Math.random()*3)+2 + player_lvl;
+//    }
+//
+//    public void generate_enemies(Game game){
+//        int num_enemies = count_num_enemies();
+//        for(int i =0; i < num_enemies; i++){
+//            game.NewEnemy()
+//        }
+//    }
+
+//}
